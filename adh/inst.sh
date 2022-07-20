ab36b0a36efd9e70a5d32c27b595258442eec1b1
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2022-07-20 15:47:43 +0800

    Update inst.sh

--- adh/inst.sh
+++ adh/inst.sh
@@ -1,1 +1,7 @@
-#!bin/bash
+#!/bin/bash
+mv ./adguardhome.conf ./adguardhome.conf.bak
+echo "127.0.0.1:253" > adguardhome.conf
+/etc/init.d/adguardhome restart
+curl https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/adh/update-adh.sh -o ./update-adh.sh
+chmod +x ./update-adh.sh
+./update-adh.sh
