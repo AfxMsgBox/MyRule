381e7461b5ae725ced2a1b1ea968c7fdc507b4c2
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2022-11-10 18:37:56 +0800

    Update inst.sh

--- adh/inst.sh
+++ adh/inst.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-if [ -f "./dns.conf" ]; then mv ./dns.conf ./dns.conf.bak; fi
-
-echo "127.0.0.1:253" > dns.conf
+if [ -f "/root/adguardhome/dns.conf" ]; then mv /root/adguardhome/dns.conf /root/adguardhome/dns.conf.bak; fi
+echo "127.0.0.1:253" > /root/adguardhome/dns.conf
 /etc/init.d/adguardhome restart
-curl https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/adh/update-adh.sh -o ./update-adh.sh
-chmod +x ./update-adh.sh
-./update-adh.sh
+
+if [ -f "/root/adguardhome/update-adh.sh" ]; then mv /root/adguardhome/update-adh.sh /root/adguardhome/update-adh.sh.bak; fi
+curl https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/adh/update-adh.sh -o /root/adguardhome/update-adh.sh
+
+chmod +x /root/adguardhome/update-adh.sh
+/root/adguardhome/update-adh.sh
+
+/etc/init.d/adguardhome restart
