08a8d6ff978f4d0e76d8a56a103082102090d43c
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2023-01-07 20:18:18 +0800

    Update update.sh

--- adh/update.sh
+++ adh/update.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if [ -f "/root/adguardhome/dns.conf" ]; then mv /root/adguardhome/dns.conf /root/adguardhome/dns.conf.bak; fi
-echo "127.0.0.1:253" > /root/adguardhome/dns.conf
-/etc/init.d/adguardhome restart
+
+#echo "127.0.0.1:253" > /root/adguardhome/dns.conf
+#/etc/init.d/adguardhome restart
 
 if [ -f "/root/adguardhome/update-adh.sh" ]; then mv /root/adguardhome/update-adh.sh /root/adguardhome/update-adh.sh.bak; fi
-curl https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/adh/update-adh.sh -o /root/adguardhome/update-adh.sh
+curl https://ghproxy.com/https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/adh/update-adh.sh -o /root/adguardhome/update-adh.sh
 
 chmod +x /root/adguardhome/update-adh.sh
 /root/adguardhome/update-adh.sh
