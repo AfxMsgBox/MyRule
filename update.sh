c60b798d989d9d3a70a2d596abfd1a1dd1fa664b
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2024-11-19 19:04:48 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+agh_dir=$(cd $(dirname $0); pwd)"/"
+
+cd $agh_dir
+
+if [ -f $agh_dir"update-agh.sh" ]; then mv $agh_dir"update-agh.sh" $agh_dir"update-agh.sh.bak"; fi
+curl --proxy http://127.0.0.1:7890 https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/agh/update-agh.sh -o $agh_dir"update-agh.sh"
+
+chmod +x $agh_dir"update-agh.sh"
+$agh_dir"update-agh.sh"
+
+if [ -f "/etc/init.d/adguardhome" ]; then
+  /etc/init.d/adguardhome restart
+fi
+
 ./agh/update.sh
 ./clash/update.sh
 
