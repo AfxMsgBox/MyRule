6279413fbc664dfa1af7c74db879519110344231
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2024-11-19 18:07:53 +0800

    Update update.sh

--- agh/update.sh
+++ agh/update.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-#agh_dir=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)"/"
 agh_dir=$(cd $(dirname $0); pwd)"/"
 
 cd $agh_dir
-
-#if [ -f $agh_dir"dns.conf" ]; then mv $agh_dir"dns.conf" $agh_dir"dns.conf.bak"; fi
-
-#echo "127.0.0.1:253" > $agh_dir"dns.conf"
-#/etc/init.d/adguardhome restart
 
 if [ -f $agh_dir"update-agh.sh" ]; then mv $agh_dir"update-agh.sh" $agh_dir"update-agh.sh.bak"; fi
 curl --proxy http://127.0.0.1:7890 https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/agh/update-agh.sh -o $agh_dir"update-agh.sh"
@@ -18,7 +12,4 @@ $agh_dir"update-agh.sh"
 
 if [ -f "/etc/init.d/adguardhome" ]; then
   /etc/init.d/adguardhome restart
-fi
-if [ -f "/etc/init.d/proxy" ]; then
-  /etc/init.d/proxy restart
 fi
