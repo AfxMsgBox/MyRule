18f0891e08e4d81ef23426e2a65f925994b78a6c
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2024-12-05 22:03:37 +0800

    Update update-config.sh

--- agh/update-config.sh
+++ agh/update-config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-URL_SCRIPT="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/agh/update.sh"
+URL_SCRIPT="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/agh/update-config.sh"
 URL_MYUPSTREAM="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/agh/myupstream.txt"
 URL_MYPROXYLIST="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/domain/myproxylist.txt"
 URL_GPT="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/domain/gpt.txt"
