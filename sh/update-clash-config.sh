547ca3fc5713104f8eae1348f3b62b260eaed18f
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2024-12-10 13:38:22 +0800

    Update and rename update-config.sh to update-clash-config.sh

---
+++ sh/update-clash-config.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DIR_SCRIPT="$(cd "$(dirname "$0")" && pwd)"
+URL_SCRIPT="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/update-clash-config.sh"
+source $DIR_SCRIPT"/common.sh"
+#----------------------------------------------------
+
+URL_CONFIG="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/clash/config.yaml"
+DIR_CONFIG=$DIR_SCRIPT"/../clash"
+#----------------------------------------------------
+echo_log "update clash config file."
+
+if [ -e "$DIR_CONFIG/local.conf" ]; then
+        if download_file $URL_CONFIG "$DIR_CONFIG/config.new" 1; then
+                replace_strings_from_config "$DIR_CONFIG/local.conf" "$DIR_CONFIG/config.new"
+                
+                mv -f "$DIR_CONFIG/config.yaml" "$DIR_CONFIG/config.yaml.bak" > /dev/null 2>&1
+                mv -f "$DIR_CONFIG/config.new"  "$DIR_CONFIG/config.yaml" > /dev/null 2>&1
+        fi
+fi
