ae44bef220c6dc5f040f8d2ca0cb2b7c707416cc
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2024-12-10 22:42:28 +0800

    Update update-all.sh

--- sh/update-all.sh
+++ sh/update-all.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
-
+#----------------------------------------------------
 DIR_SCRIPT=$(dirname "$(readlink -f "$0")")
+#直接调用common.sh，来升级common.sh
 sh $DIR_SCRIPT"/common.sh"
 
 URL_SCRIPT="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/update-all.sh"
+#引用common.sh来升级自己
 source $DIR_SCRIPT"/common.sh"
 
+#----------------------------------------------------
 URL_UPDATE_AGH="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/update-agh-config.sh"
 URL_UPDATE_CLASH="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/update-clash-config.sh"
 URL_UPDATE_META="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/update-meta-config.sh"
 URL_UPDATE_PROXY_RULE="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/update-proxy-rule.sh"
 URL_KEEPLIVE="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/keeplive.sh"
+
 #----------------------------------------------------
 echo_log "update keeplive.sh"
 download_file $URL_KEEPLIVE $DIR_SCRIPT"/keeplive.sh" 1
