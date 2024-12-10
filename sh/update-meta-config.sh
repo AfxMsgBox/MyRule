72d07791725f9696d9fdc7fb02571c3cee999e92
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2024-12-10 12:18:02 +0800

    Update update-meta-config.sh

--- sh/update-meta-config.sh
+++ sh/update-meta-config.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
-URL_SCRIPT="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/update-meta-config.sh"
-URL_CONFIG="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/meta/config.yaml"
 
 DIR_SCRIPT="$(cd "$(dirname "$0")" && pwd)"
 source $DIR_SCRIPT"/common.sh"
 
+URL_SCRIPT="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/update-meta-config.sh"
+URL_CONFIG="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/meta/config.yaml"
+DIR_CONFIG=$DIR_SCRIPT"../meta"
 #----------------------------------------------------
 echo_log "update clash config file."
 
-if [ -e "$DIR_SCRIPT/local.conf" ]; then
-        if download_file $URL_CONFIG "$DIR_SCRIPT/config.new" 1; then
-                replace_strings_from_config "$DIR_SCRIPT../meta/local.conf" "$DIR_SCRIPT/config.new"
+if [ -e "$DIR_CONFIG/local.conf" ]; then
+        if download_file $URL_CONFIG "$DIR_CONFIG/config.new" 1; then
+                replace_strings_from_config "$DIR_CONFIG/local.conf" "$DIR_CONFIG/config.new"
                 
-                mv -f "$DIR_SCRIPT/../meta/config.yaml" "$DIR_SCRIPT/../meta/config.yaml.bak" > /dev/null 2>&1
-                mv -f "$DIR_SCRIPT/config.new" "$DIR_SCRIPT/../meta/config.yaml" > /dev/null 2>&1
+                mv -f "$DIR_CONFIG/config.yaml" "$DIR_CONFIG/config.yaml.bak" > /dev/null 2>&1
+                mv -f "$DIR_CONFIG/config.new"  "$DIR_CONFIG/config.yaml" > /dev/null 2>&1
         fi
 fi
