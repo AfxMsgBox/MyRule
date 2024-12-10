03b7bec75f4ed1265686b3f5254cfccdfeb8f0d2
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2024-12-10 13:24:23 +0800

    Update update-proxy-rule.sh

--- sh/update-proxy-rule.sh
+++ sh/update-proxy-rule.sh
@@ -1,29 +1,7 @@
 #!/bin/sh
 URL_SCRIPT="https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/update-proxy-rule.sh"
-
 DIR_SCRIPT="$(cd "$(dirname "$0")" && pwd)"
 source $DIR_SCRIPT"/common.sh"
-#----------------------------------------------------
-SED_EXPR=""
-
-replace_strings_from_config() {
-    CONFIG_FILE="$1"
-
-    # 从配置文件中读取所有的键值对并构建 sed 表达式
-    while IFS='=' read -r key value; do
-        # 跳过空行或无效行
-        if [ -z "$key" ] || [ -z "$value" ]; then
-            continue
-        fi
-
-        # 转义特殊字符
-        ESCAPED_KEY=$(printf '%s\n' "$key" | sed -e 's/[][\/.^$*]/\\&/g')
-        ESCAPED_VALUE=$(printf '%s\n' "$value" | sed -e 's/[&\\/]/\\&/g')
-
-        # 构建 sed 表达式
-        SED_EXPR="${SED_EXPR}s|{${ESCAPED_KEY}}|${ESCAPED_VALUE}|g;"
-    done < "$CONFIG_FILE"
-}
 
 #----------------------------------------------------
 echo_log "update clash proxy & rules."
