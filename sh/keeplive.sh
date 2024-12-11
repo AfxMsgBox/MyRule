f08b51d3b6bf6d248ce7fc786186b833c7de3fed
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2024-12-11 14:59:50 +0800

    Update keeplive.sh

--- sh/keeplive.sh
+++ sh/keeplive.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+#*/5 * * * *  sh /etc/proxy/sh/keeplive.sh
 curl --connect-timeout 5 --proxy http://127.0.0.1:7890 -I https://www.google.com > /dev/null 2>&1
 curl --connect-timeout 5 --proxy http://127.0.0.1:7890 -I https://www.chatgpt.com > /dev/null 2>&1
 
