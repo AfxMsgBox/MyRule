ed71e71ee4c4324a14cd37338dbd030926eb2d09
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2024-12-12 23:11:44 +0800

    Update inst.sh

--- sh/inst.sh
+++ sh/inst.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #curl -X POST http://127.0.0.1:3721/cache/fakeip/flush && ping www.google.com
-wget -O- https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/download-all-script.sh|sh
+wget -O- https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/download-all-scripts.sh|sh
