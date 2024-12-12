d42378004370b00ed2386e72142fd1be788fa384
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2024-12-12 11:23:14 +0800

    Update inst.sh

--- sh/inst.sh
+++ sh/inst.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget -O- https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/inst.sh|sh
+wget -O- https://raw.githubusercontent.com/AfxMsgBox/MyRule/main/sh/download-all-script.sh|sh
